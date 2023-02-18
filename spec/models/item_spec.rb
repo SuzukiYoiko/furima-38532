744ba6b10965f923fb3520fdd3ca9ef020e7b201
SuzukiYoiko <113274467+SuzukiYoiko@users.noreply.github.com>
2023-02-18 17:24:02 +0900

    Update item_spec.rb

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -71,6 +71,10 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Product price must be less than or equal to 9999999")
       end
+      it 'userが紐づいていなければ登録できない' do
+        @item.user_id = nil
+        expect(@item.errors[:user_id]).to include()
+      end
     end
   end
 end
