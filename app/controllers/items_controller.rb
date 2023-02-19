11c56305b27d45738602d1d8201b037c3373e411
SuzukiYoiko <113274467+SuzukiYoiko@users.noreply.github.com>
2023-02-19 18:09:05 +0900

    商品情報変種機能の修正
    #WHAT
    商品情報編集機能の修正
    #WHY
    指摘事項修正のため

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,8 @@
 class ItemsController < ApplicationController
   
   before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_item, only: [:edit, :update, :show]
   before_action :move_to_index, only: [:edit]
-  before_action :set_tweet, only: [:edit, :update, :show]
 
   def index
     @items = Item.all.order(created_at: :desc)
